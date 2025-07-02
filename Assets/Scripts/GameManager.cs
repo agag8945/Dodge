4dab13e2c7d37f9cdab8b8829f6e9d1753caa2b2
DESKTOP-N7S62IG\202_05 <kms020824@naver.com>
2025-07-02 15:42:12 +0900

    게임 매니져, 레벨 회전 스크립트 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 관련 라이브러리
+using UnityEngine.SceneManagement; // 씬 관리 관련 라이브러리
+
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject gameoverText; // 게임 오버 시 활성화 할 텍스트 게임 오브젝트
+    public Text timeText;           // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text recordText;        // 최고 기록을 표시할 텍스트 컴포넌트    
+
+    private float surviveTime;      // 생존 시간
+    private bool isGameOver;        // 게임 오버 상태
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // 생존 시간과 게임 오버 상태 초기화
+        surviveTime = 0;
+        isGameOver = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 게임오버가 아닐 때
+        if (!isGameOver)
+        {
+            // 생존 시간 갱신
+            surviveTime += Time.deltaTime;
+
+            // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 통해 표시
+            timeText.text = "Time : " + (int)surviveTime;
+        }
+
+        else
+        {
+            // 게임 오버인 상태에서 키보드 R키를 누르면
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // 현재 씬 다시 로드
+                SceneManager.LoadScene("Main");
+            }
+        }
         
     }
 
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+
+    public float rotationSpeed = 60f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,7 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.Rotate(0f, rotationSpeed, 0);
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0);
     }
 }
